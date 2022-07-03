17ceaaed4a7054aae6ed062aca4a46fad9d97f8e
seif <sifelkateb@gmail.com>
2022-07-03 22:57:29 +0200

    add user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "the username must be provided"],
+    minLength: 4,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "the email must be provided"],
+    match: [
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+      "you must enter a valid email ",
+    ],
+    unique:true
+  },
+  password:{
+    type:String,
+    required:[true,'password must be provided'],
+    minLength:8
+  }
+});
+
+const UserModel=mongoose.model('User',userSchema);
+
+
+module.exports=UserModel
