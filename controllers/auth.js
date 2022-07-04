7e5349d050f0522520c3cdb2fa61bda15f1cbd3c
seif <sifelkateb@gmail.com>
2022-07-04 11:23:26 +0200

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,33 @@
-const { StatusCodes } = require("http-status-codes")
+const { StatusCodes } = require("http-status-codes");
+const UserModel = require("../models/user");
 
+const bcrypt=require('bcryptjs');
+const { BadRequestError } = require("../errors");
 
 
 
 const registerUser=async(req,res,next)=>{
-    res.status(StatusCodes.CREATED).json({msg:'registered User'})
+
+
+    const {name,password,email}=req.body;
+
+    if(!password||!email||!name){
+        throw new BadRequestError('you must provide name,email and password');
+    }
+
+
+    const salt=await bcrypt.genSalt(10);
+
+    const hashPassword=await bcrypt.hash(password,salt);
+
+    const tempUser={name,password:hashPassword,email};
+    const user=await UserModel.create(tempUser);
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 
 const loginUser=async(req,res,next)=>{
+    
     res.status(StatusCodes.OK).json({msg:'logged User'})
 }
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,9 +4,10 @@ const { CustomError } = require("../errors")
 
 const errorHandlerMiddleware=(err,req,res,next)=>{
     if(err instanceof CustomError){
-        return res.status(err.stausCode).json({msg:err.message})
+        return res.status(err.statusCode).json({msg:err.message})
     }
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:err})
+    console.log(err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:err.message})
 }
 
 module.exports=errorHandlerMiddleware
