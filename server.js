e0821aa6d703a5a7448d132946d99d5f427783e9
seif <sifelkateb@gmail.com>
2022-07-05 23:42:11 +0200

    set security packages

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
 const express=require('express');
+
+const helmet=require('helmet');
+
+const cors=require('cors');
+
+const xss=require('xss-clean');
+
+const rateLimit=require('express-rate-limit');
 const { StatusCodes } = require('http-status-codes');
 const morgan=require('morgan');
 const authenticateUser = require('./middleware/auth');
@@ -16,7 +24,16 @@ const port =process.env.PORT||3000;
 
 //middleware
 
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
+
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'));
 
