19524bf89c3fd1b7eb943fddef31ca0fb08472eb
seif <sifelkateb@gmail.com>
2022-07-05 22:55:39 +0200

    duplicate email error setup

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,26 @@
-const { StatusCodes } = require("http-status-codes")
+const { StatusCodes } = require("http-status-codes");
+const { object } = require("joi");
 const { CustomError } = require("../errors")
 
 
 const errorHandlerMiddleware=(err,req,res,next)=>{
-    if(err instanceof CustomError){
-        return res.status(err.statusCode).json({msg:err.message})
+    // if(err instanceof CustomError){
+    //     return res.status(err.statusCode).json({msg:err.message})
+    // }
+
+
+
+    const customError={
+        message:err.message||'Error occured at the server please try again later',
+        statusCode:err.statusCode||StatusCodes.INTERNAL_SERVER_ERROR
+    }
+    if(err.code=='11000'){
+        console.log('yea yea yea yea ')
+        customError.message='The '+Object.keys(err.keyValue).map(key=>`${key}`)+' you provided '+(Object.keys(err.keyValue).length>1?'are':'is')+' already taken';
+        customError.statusCode=StatusCodes.BAD_REQUEST
     }
     console.log(err);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:err.message})
+    res.status(customError.statusCode).json({msg:customError.message})
 }
 
 module.exports=errorHandlerMiddleware
